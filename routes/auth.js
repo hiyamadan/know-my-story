2f2117394c8369c97ea98cba73218c62d81ffe1c
Hiya Madan <madan@Dell-Hiya.(none)>
2022-07-21 15:52:46 +0530

    added login, views,database setup,landing page,routes,models

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+//Auth with Google
+//GET /auth/google
+router.get('/google', 
+            passport.authenticate('google', { scope: ['profile'] })
+          )
+
+//Google auth callback
+//GET /auth/google/callback
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/dashboard')
+    }
+  )
+
+//Logout user
+//GET /auth/logout
+router.get('/logout', (req, res,next) => {
+    req.logout((err)=>{
+        if(err)
+        {
+            return next(err);
+        }
+    })
+    res.redirect('/')
+  })
+
+  module.exports = router ;
